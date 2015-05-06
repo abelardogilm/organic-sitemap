8251734ae94218795c3874cf34b84621e194546b
Pablico <pablo.molinacandel@gmail.com>
2015-05-06 17:45:19 +0200

    sitemap specific domains configuration

--- lib/organic-sitemap/middleware/url_capture.rb
+++ lib/organic-sitemap/middleware/url_capture.rb
@@ -7,26 +7,29 @@ module OrganicSitemap
 
       def call(env)
         status, headers, response = @app.call(env)
-        if healthy_url(status, headers, response, env)
+        request = Rack::Request.new env
+        if sitemap_url(status, headers, response, request, env)
           OrganicSitemap::RedisManager.add(env['REQUEST_PATH'])
         end
         [status, headers, response]
       end
 
-      def healthy_url(status, headers, response, env)
-        success_response(status) && html_page(headers) && get_method?(env)
+      def sitemap_url(status, headers, response, request, env)
+        success_response(status) && html_page(response) && request.get? && is_expected_domain?(request)
       end
 
       def success_response(status)
         status == 200
       end
 
-      def html_page(headers)
-        headers["Content-Type"].include? "text/html"
+      def html_page(response)
+        response.content_type.include? "text/html"
       end
 
-      def get_method?(env)
-        env['REQUEST_METHOD'] == 'GET'
+      def is_expected_domain?(request)
+        # Any domain if not explicitly configured
+        return true if !OrganicSitemap.domains || OrganicSitemap.domains.empty?
+        OrganicSitemap.domains.include? request.host
       end
     end
   end
