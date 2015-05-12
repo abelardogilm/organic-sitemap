e619700cac25cf76f39fb898521f92d27150b6fe
Abelardo <abelardogilm@gmail.com>
2015-05-12 10:17:26 +0200

    Merge with master

--- lib/organic-sitemap.rb
+++ lib/organic-sitemap.rb
@@ -9,9 +9,10 @@ end
 module OrganicSitemap
   extend Configuration
 
-  define_setting :storage,          "redis"
-  define_setting :key,              "sitemap-urls"
+  define_setting :storage,          'redis'
+  define_setting :redis_key,        'sitemap-urls'
   define_setting :domains
+  define_setting :skipped_params,   ['utm']
   define_setting :redis_connection, Redis.new(url: 'redis://127.0.0.1:6379')
   define_setting :expiry_time,      7.days
 end
