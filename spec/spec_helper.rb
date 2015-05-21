e24ce5ff72d1a631b2c12907fc41aa37c7f0d155
Abelardo <abelardogilm@gmail.com>
2015-05-21 21:49:44 +0200

    Add require on tests and prepare constants with response status

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,17 @@ require 'redis'
 
 SimpleCov.start
 
+require 'fakeredis/rspec'
+
 RSpec.configure do |config|
   config.filter_run_excluding broken: true
 end
+
+OrganicSitemap.configure
+
+OK_STATUS_CODES = 200..207
+REDIRECT_STATUS_CODES = 300..307
+CLIENT_ERROR_STATUS_CODES = 400..429
+ERROR_STATUS_CODE = 500..509
+
+ALL_STATUS = [*OK_STATUS_CODES, *REDIRECT_STATUS_CODES, *CLIENT_ERROR_STATUS_CODES, *ERROR_STATUS_CODE]
