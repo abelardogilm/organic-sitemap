b994d7b63ce4de74b8d598e453c1ce6ae0c8f516
Abelardo <abelardogilm@gmail.com>
2015-11-27 14:37:37 +0100

    More info on crawler_manager response, valid and invalid urls

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,15 +2,15 @@
 
 ### Changed
 
-- Let crawler_service accept Httparty options
+- Let crawler_manager accept Httparty options
 
 ## 0.1.3
 
 ### Changed
 
-- Add cache_service for get expiry cache url
+- Add cache_manager for get expiry cache url
 - Fix error with rspec gem versions
-- Add crawler_service to warmup urls
+- Add crawler_manager to warmup urls
 
 ## 0.1.2
 
--- lib/organic-sitemap/crawler_manager.rb
+++ lib/organic-sitemap/crawler_manager.rb
@@ -1,18 +1,30 @@
 module OrganicSitemap
   class CrawlerManager
     def self.warmup(urls, opts = {})
-      [*urls].each do |uri|
+      data = {urls: [*urls],
+              valids: 0,
+              invalids: {}}
+
+      data[:urls].each do |uri|
         p "OrganicSitemap::CrawlerManager GET: #{uri}"
         response = HTTParty.get(url_for(uri), opts)
 
         url_processed = OrganicSitemap::UrlProcessor.new(response.code, response.headers, response.request)
 
         unless url_processed.url_from_cache_valid?
-          p "OrganicSitemap::CrawlerManager REMOVING URL: #{uri} invalid url"
+          p "OrganicSitemap::CrawlerManager REMOVING URL: #{uri} response code: #{response.code}"
           OrganicSitemap::RedisManager.remove_key(key: uri)
+          if data[:invalids][response.code]
+            data[:invalids][response.code] << uri
+          else
+            data[:invalids][response.code] = [uri]
+          end
+        else
+          data[:valids] += 1
         end
         sleep crawler_delay if crawler_delay
       end
+      data
     end
 
     private
@@ -20,6 +32,7 @@ module OrganicSitemap
     def self.url_for(uri)
       "#{OrganicSitemap.configuration.crawler_domain}#{uri}"
     end
+
     def self.crawler_delay
       OrganicSitemap.configuration.crawler_delay
     end
